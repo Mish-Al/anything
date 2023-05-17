90f279053d03bc0708c492f36033c28fbd20258a
Mish-Al <mtaliep.lifechoices@gmail.com>
2023-05-17 10:16:31 +0200

    day 4 exercise

---
+++ script/code.js
@@ -0,0 +1,12 @@
+let person1 = {
+    firstName: 'Mish-Al' 
+};
+
+let person2 = {
+    name: 'Bahijah'
+};
+console.log(person1);
+console.log(person2);
+
+let family = [person1, person2];
+console.log(family)
